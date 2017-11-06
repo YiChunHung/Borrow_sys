13893154215815d764d2540cccb7aedfa4e9b205
balloliver <b102626b@gmail.com>
2017-11-07 00:36:03 +0800

    only one item can be render correctly

--- src/components/TimeTable.jsx
+++ src/components/TimeTable.jsx
@@ -4,56 +4,117 @@ import ButtonTimeAvailable from 'components/ButtonTimeAvailable.jsx';
 import ButtonTimeDelete from 'components/ButtonTimeDelete.jsx';
 import 'components/TimeTable.css';
 
+function ButtonChoosing(props){
+	if (props.notAvailable) {
+		return (
+			<ButtonTimeDelete item={props.item}/>
+		)
+	} else {
+		return (
+			<ButtonTimeAvailable/>
+		)
+	}
+}
+
+
+
 export default class TimeTable extends React.Component{
+	constructor(){
+		super();
+		this.state = {
+			frontDate: [{
+				id: 1,
+				date:'11/01'
+			},{
+				id: 2,
+				date:'11/03'
+			}/*,{
+				date:
+			},{
+				date:
+			},{
+				date:
+			},{
+				date:
+			},{
+				date:
+			}*/],
+			back:[{
+				id: 3,
+				item: 'pen',
+				date: '11/01',
+				morning: true,
+				afternoon: true,
+				night: false
+			}, {
+				id: 4,
+				item: 'pen',
+				date: '11/03',
+				morning: false,
+				afternoon: true,
+				night: true
+			}, {
+				id: 5,
+				item: 'room',
+				date: '11/01',
+				morning: false,
+				afternoon: true,
+				night: true
+			}]
+
+
+		}
+	}
+
+
 
 	render(){
+		const head = this.state.frontDate.map(
+			front => <td key={front.id}> {front.date} </td>
+		);
+		const rowMorning = this.state.back.map(
+			morning => 	
+				<td key={morning.id}>
+				<ButtonChoosing notAvailable={morning.morning} item={morning.item}/>
+				</td>
+		);
+		const rowAfternoon = this.state.back.map(
+			afternoon => 	
+				<td key={afternoon.id}>
+				<ButtonChoosing notAvailable={afternoon.afternoon} item={afternoon.item}/>
+				</td>
+		);
+		const rowNight = this.state.back.map(
+			night => 	
+				<td key={night.id}>
+				<ButtonChoosing notAvailable={night.night} item={night.item}/>
+				</td>
+		);
+
+
 		return (
-     		<Table bordered>
-        		<thead>
-          			<tr>
-            		<th> Time interval </th>
-            		<th> Date1 </th>
-            		<th> Date2 </th>
-            		<th> Date3 </th>
-            		<th> Date4 </th>
-            		<th> Date5 </th>
-            		<th> Date6 </th>
-            		<th> Date7 </th>
-          			</tr>
-	        	</thead>
-	        	<tbody>
-	          		<tr>
-	            	<th scope="row">Morning</th>
-	            	<td> <ButtonTimeAvailable/> </td>
-	            	<td> <ButtonTimeAvailable/> </td>
-	            	<td> <ButtonTimeDelete item="pen"/> <ButtonTimeDelete item="projector"/> </td>
-	            	<td> <ButtonTimeAvailable/> </td>
-	            	<td> <ButtonTimeDelete item="desk"/> </td>
-	            	<td> <ButtonTimeAvailable/> </td>
-	            	<td> <ButtonTimeDelete item="desk"/> </td>
-	          		</tr>
-	          		<tr>
-	            	<th scope="row">Afternoon</th>
-	            	<td> <ButtonTimeDelete item="desk"/> <ButtonTimeDelete item="pen"/> <ButtonTimeDelete item="projector"/> </td>
-	            	<td> <ButtonTimeDelete item="desk"/> </td>
-	            	<td> <ButtonTimeAvailable/></td>
-	            	<td> <ButtonTimeAvailable/></td>
-	            	<td> <ButtonTimeDelete item="printer"/> </td>
-	            	<td> <ButtonTimeDelete item="desk"/> </td>
-	            	<td> <ButtonTimeAvailable/></td>
-	          		</tr>
-	          		<tr>
-	            	<th scope="row">Night</th>
-	            	<td> <ButtonTimeAvailable/> </td>
-	            	<td> <ButtonTimeDelete item="meeting room"/> <ButtonTimeDelete item="desk"/> </td>
-	            	<td> <ButtonTimeDelete item="table"/> </td>
-	            	<td> <ButtonTimeAvailable/></td>
-	            	<td> <ButtonTimeAvailable/></td>
-	            	<td> <ButtonTimeAvailable/></td>
-	            	<td> <ButtonTimeAvailable/></td>
-	          		</tr>
-	        	</tbody>
-      		</Table>
+     		<table>
+     			<thead>
+     				<tr>
+     					<td>Date</td>
+     					{head}
+     				</tr>
+     			</thead>
+     			<tbody>
+     				<tr>
+     					<td>Morning</td>
+     					{rowMorning}
+     				</tr>
+     				<tr>
+     					<td>Afternoon</td>
+     					{rowAfternoon}
+     				</tr>
+     				<tr>
+     					<td>Night</td>
+     					{rowNight}
+     				</tr>
+     			</tbody>
+     		</table>
     	);
 	};
 }
