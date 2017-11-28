5163df92237603363d43d7fe986bbcf91b119108
winston1628 <qwertyuiop851689@yahoo.com.tw>
2017-11-28 17:46:14 +0800

    handmade calendar

--- src/components/Borrowpage.jsx
+++ src/components/Borrowpage.jsx
@@ -3,9 +3,58 @@ import moment from 'moment'
 import Calendar from 'components/Calendar.jsx'
 
 export default class Borrowpage extends React.Component {
+	
+	constructor(props){
+		super(props);
+		this.state = {
+			date: moment(),
+			selectedDays: []
+		};
+		this.toggle = this.toggle.bind(this);
+	}
+
+
+	toggle(e) {
+		const className = e.target.className;
+		const isPicked = this.state.selectedDays.includes(className);
+		
+		if (this.state.selectedDays.length <= 6){
+			if (isPicked){
+				this.setState({
+					selectedDays: this.state.selectedDays.filter(function(item){
+										return item != className
+									})
+				})
+			}
+			else{
+				this.setState({
+					selectedDays: [...this.state.selectedDays, className]
+				})
+
+			}
+		}
+		else{
+			if (isPicked){
+				this.setState({
+					selectedDays: this.state.selectedDays.filter(function(item){
+										return item != className
+									})
+				})
+			}
+			else{
+				alert('選擇天數不能超過七天')
+			}
+		}
+	}
+
 	render(){
 		return(
-			<Calendar />
+			<Calendar 
+				onChangeMonth={(date) => this.setState({date})}
+				date = {this.state.date}
+				choosedDays = {this.toggle}
+				selectedDays = {this.state.selectedDays}
+			/>
 		);
 	}	
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,19 +3,7 @@ import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } f
 import Borrowpage from 'components/Borrowpage.jsx'
 
 export default class Main extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false
-    };
-  }
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
   render() {
     return (
       <div>
