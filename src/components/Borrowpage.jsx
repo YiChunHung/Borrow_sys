e36210353af1502996607317b2b8f0b8b4be9120
winston1628 <qwertyuiop851689@yahoo.com.tw>
2017-12-22 21:02:26 +0800

    Complete Item List

--- src/components/Borrowpage.jsx
+++ src/components/Borrowpage.jsx
@@ -1,40 +1,61 @@
 import React from 'react';
+import axios from 'axios';
 import ItemList from 'components/ItemList.jsx';
 import ITEM from 'components/item.json';
 
 export default class Borrowpage extends React.Component {
   constructor(props) {
     super(props);
+    axios({
+      method:'get',
+      url:'http://140.114.84.187:5000/api/items/read',
+      params: {'operator_uid':0},
+      headers: {
+        'Access-Control-Allow-Origin': 'http://140.114.84.187:5000/',
+      },
+      timeout: 1000,
+      "routes": {"cors": true}
+    })
+    .then(function(response) {
+      console.log(response)
+    });
     this.state = {
-    	selectedItems: []
+    	selectedItems: [],
+      item: ['- Select -', ...ITEM.item[0].name]
     };
     this.toggle = this.toggle.bind(this);
+    this.restoreItems = this.restoreItems.bind(this);
+  }
+  
+  toggle(item) {
+    const isPicked = this.state.selectedItems.includes(item);
+
+    this.setState({
+      item: this.state.item.filter(function(stuff){return(stuff != item)})
+    })
+  
+    if (isPicked != 'True') {
+      this.setState({
+        selectedItems: [...this.state.selectedItems, item]
+      })
+    }  
   }
 
-  toggle(e) {
-  	const className = e.target.className;
-  	const isPicked = this.state.selectedItems.includes(className);
-
-  	if(isPicked){
-  		this.setState({
-  			selectedItems: this.state.selectedItems.filter(function(item){return item != className})
-  		})
-  	}
-  	else{
-  		this.setState({
-  			selectedItems: [...this.state.selectedItems, className]
-  		})
-  	}
+  restoreItems(item) {
+    this.setState({
+      item: [...this.state.item, item],
+      selectedItems: this.state.selectedItems.filter(function(stuff){return(stuff != item)})
+    })
   }
 
   render() {
-  	const item = ITEM.item[0].name;
     return (
       <div>
       	<ItemList 
       		choosedItems = {this.toggle}
       		selectedItems = {this.state.selectedItems}
-      		item = {item}
+      		item = {this.state.item}
+          restoreItems = {this.restoreItems}
       	/>
       </div>
     );
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,59 +1,62 @@
 import React from 'react';
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import { Col, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import 'components/itemList.css'
 
 export default class ItemList extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      collapsed: true,
-    };
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.itemList = this.itemList.bind(this);
+    this.showList = this.showList.bind(this);
+    this.showSelectedList = this.showSelectedList.bind(this);
     this.toggle = this.toggle.bind(this);
-  }
-
-  toggleNavbar() {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
+    this.restoreItem = this.restoreItem.bind(this);
   }
 
   toggle(e) {
-    this.props.choosedItems(e)
+    var item = document.getElementById("exampleSelect").value
+    this.props.choosedItems(item)
   }
 
-  itemList(item) {    
+  restoreItem(e) {
+    var item = e.target.className
+    this.props.restoreItems(item)
+  }
+
+  showList(item) {
     return(
-      <NavItem
-        key={item}
-      >
-        <button
-          style={{background: this.props.selectedItems.includes(item)? "blue":"white"}}
-          onClick={this.toggle}
-          className={item}
-        >
-          {item}
+      <option key={item} className="singleItem">
+        {item}
+      </option>
+    )
+  }
+
+  showSelectedList(item) {
+    return(
+      <p key={item} className="selectedItems">
+        {item}
+        <button 
+          className={item} 
+          onClick={this.restoreItem}
+          key={item}
+        > 
+          x
         </button>
-      </NavItem>
-    );
-    
+      </p>
+    )
   }
 
   render() {
     return (
       <div>
-        <Navbar color="faded" light>
-          <NavbarBrand> 
-            <button onClick={this.toggleNavbar} className="mr-auto">
-              Item List
-            </button>
-          </NavbarBrand>
-          <Collapse isOpen={!this.state.collapsed} navbar>
-            <Nav navbar>
-              {this.props.item.map(this.itemList)}
-            </Nav>
-          </Collapse>
-        </Navbar>
+        <h2>Selected Item List</h2>
+        {this.props.selectedItems.map(this.showSelectedList)}
+        <Form>
+          <FormGroup>
+            <Input type="select" className="itemList" id="exampleSelect" onChange={this.toggle}>
+              {this.props.item.map(this.showList)}
+            </Input>
+          </FormGroup>
+        </Form>
       </div>
     );
   }
