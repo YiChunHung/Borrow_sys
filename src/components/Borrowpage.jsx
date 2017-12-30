27da2a1936a146d11fe49f21de58a5ee4c431a0c
winston1628 <qwertyuiop851689@yahoo.com.tw>
2017-12-30 19:31:10 +0800

    Nothing

--- src/components/Borrowpage.jsx
+++ src/components/Borrowpage.jsx
@@ -1,24 +1,55 @@
 import React from 'react';
 import axios from 'axios';
+import moment from 'moment'
 import ItemList from 'components/ItemList.jsx';
+import Calendar from 'components/Calendar.jsx';
+import TimeTable from 'components/TimeTable.jsx';
+import 'components/Borrowpage.css'
 import ITEM from 'components/item.json';
 
 export default class Borrowpage extends React.Component {
   constructor(props) {
     super(props);
-    var item = this.loadData()
-    console.log(item)
+
+    const ax = axios.create({
+      baseURL: 'http://localhost:7070/components'
+    })
+    ax.get('testdata.json')
+    /*
+    axios({
+      method:'get',
+      url:'/Users/winston/Desktop/Borrow_sys/src/components/testdata.json',
+      params: {'operator_uid':0},
+      headers: {
+        'Access-Control-Allow-Origin': 'http://140.114.84.187:5000/',
+      },
+      timeout: 1000,
+      "routes": {"cors": true}
+    })
+    .then(function(response) {
+      var item_iid = response.data.payload.map(function(item){return(item.iid)}); 
+      var item_name = response.data.payload.map(function(item){return(item.item_name)});
+      var item = response.data.payload.map(function(item){return [item.iid, item.item_name]});
+      //console.log(item_iid)
+      //console.log(item_name)
+      self.setState({id: item_iid, name: item_name})
+      return item;
+    });
+    */
     this.state = {
+      date: moment(),
+      selectedDays: [],
     	selectedItems: [],
       item: ['- Select -', ...ITEM.item[0].name],
       //item: this.loadData()
     };
-    this.toggle = this.toggle.bind(this);
+    this.toggleCalendar = this.toggleCalendar.bind(this);
+    this.toggleItemList = this.toggleItemList.bind(this);
     this.restoreItems = this.restoreItems.bind(this);
-    this.loadData = this.loadData.bind(this);
+    //this.loadData = this.loadData.bind(this);
   }
   
-  toggle(item) {
+  toggleItemList(item) {
     const isPicked = this.state.selectedItems.includes(item);
 
     this.setState({
@@ -32,6 +63,30 @@ export default class Borrowpage extends React.Component {
     }  
   }
 
+  toggleCalendar(e) {
+    const className = e.target.className;
+    const isPicked = this.state.selectedDays.includes(className);
+    
+    
+    if (isPicked){
+      this.setState({
+        selectedDays: this.state.selectedDays.filter(function(item){
+                        return item != className
+                      })
+      })
+    }
+    else{
+      if (this.state.selectedDays.length <= 6){
+        this.setState({
+          selectedDays: [...this.state.selectedDays, className]
+        })
+      }
+      else {
+        alert('選擇天數不可超過七天')
+      }
+    }
+  }
+  /*
   loadData() {
     var self = this;
     return axios({
@@ -54,7 +109,7 @@ export default class Borrowpage extends React.Component {
       return item;
     });
   }
-
+  */
   restoreItems(item) {
     this.setState({
       item: [...this.state.item, item],
@@ -65,12 +120,28 @@ export default class Borrowpage extends React.Component {
   render() {
     return (
       <div>
-      	<ItemList 
-      		choosedItems = {this.toggle}
-      		selectedItems = {this.state.selectedItems}
-      		item = {this.state.item}
-          restoreItems = {this.restoreItems}
-      	/>
+        <div id = "ItemList">
+        	<ItemList 
+        		choosedItems = {this.toggleItemList}
+        		selectedItems = {this.state.selectedItems}
+        		item = {this.state.item}
+            restoreItems = {this.restoreItems}
+        	/>
+        </div>
+        <div id = "Calendar"> 
+          <Calendar
+            onChangeMonth={(date) => this.setState({date})}
+            date = {this.state.date}
+            choosedDays = {this.toggleCalendar}
+            selectedDays = {this.state.selectedDays}
+          />
+        </div>
+        <div id = "TimeTable">  
+          <TimeTable 
+            frontDate={this.state.selectedDays}
+            item={this.state.selectedItems}
+          />
+        </div>
       </div>
     );
   }
---
+++ src/components/ButtonTimeAvailable.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import axios from 'axios';
+import "components/ButtonTimeAvailable.css";
+
+export default class ButtonTimeAvailable extends React.Component{
+	constructor(props){
+		super(props);
+
+		this.toggle = this.toggle.bind(this);
+		this.state = {
+			isSelected: false
+		};
+
+	}
+
+	toggle(){
+		//console.log(this.props.onChange);
+		this.setState(
+			{isSelected: !this.state.isSelected}
+		);
+		this.props.onChange(this.props.time);
+
+		axios({
+			method:'get',
+			url:'http://140.114.84.187:5000/api/items/read',
+			params:{'operator_uid':0},
+			headers:{'Access-Control-Allow-Origin':'http://140.114.84.187:5000'}
+			//"routes": {"cors": true}
+		})
+		.then(
+			function(response){
+				console.log(response);
+			}
+		);
+
+		/*var API = axios({
+			//method:'get',
+			url:'http://140.114.84.187:5000/api/items/read',
+			headers: {"Access-Control-Allow-Origin": "*"}
+		});*/
+		/*var API = axios.get(
+			'http://140.114.84.187:5000/api/items/read',{
+				headers:{"Access-Control-Allow-Origin": "*"}
+			}
+		) 
+
+		console.log(API);*/
+	}
+
+	render(){
+		return(
+			
+				<button style={{backgroundColor: this.state.isSelected? "yellow":"orange"}} className="button_1" onClick={this.toggle}>
+					{this.state.isSelected ? "Ｖ":"　"}
+				</button>
+			
+		)
+	}
+}
