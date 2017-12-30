6672c4ff2bcfa21d1898e57b3967093ab6722f72
winston1628 <qwertyuiop851689@yahoo.com.tw>
2017-12-30 14:21:37 +0800

    try merge

--- src/components/Borrowpage.jsx
+++ src/components/Borrowpage.jsx
@@ -6,26 +6,16 @@ import ITEM from 'components/item.json';
 export default class Borrowpage extends React.Component {
   constructor(props) {
     super(props);
-    axios({
-      method:'get',
-      url:'http://140.114.84.187:5000/api/items/read',
-      params: {'operator_uid':0},
-      headers: {
-        'Access-Control-Allow-Origin': 'http://140.114.84.187:5000/',
-      },
-      timeout: 1000,
-      "routes": {"cors": true}
-    })
-    .then(function(response) {
-      var item_iid = response.data.payload.map(function(item){return(item.iid)}) 
-      var item_name = response.data.payload.map(function(item){return(item.item_name)})
-    });
+    var item = this.loadData()
+    console.log(item)
     this.state = {
     	selectedItems: [],
-      item: ['- Select -', ...ITEM.item[0].name]
+      item: ['- Select -', ...ITEM.item[0].name],
+      //item: this.loadData()
     };
     this.toggle = this.toggle.bind(this);
     this.restoreItems = this.restoreItems.bind(this);
+    this.loadData = this.loadData.bind(this);
   }
   
   toggle(item) {
@@ -40,6 +30,29 @@ export default class Borrowpage extends React.Component {
         selectedItems: [...this.state.selectedItems, item]
       })
     }  
+  }
+
+  loadData() {
+    var self = this;
+    return axios({
+      method:'get',
+      url:'http://140.114.84.187:5000/api/items/read',
+      params: {'operator_uid':0},
+      headers: {
+        'Access-Control-Allow-Origin': 'http://140.114.84.187:5000/',
+      },
+      timeout: 1000,
+      "routes": {"cors": true}
+    })
+    .then(function(response) {
+      var item_iid = response.data.payload.map(function(item){return(item.iid)}); 
+      var item_name = response.data.payload.map(function(item){return(item.item_name)});
+      var item = response.data.payload.map(function(item){return [item.iid, item.item_name]});
+      //console.log(item_iid)
+      //console.log(item_name)
+      self.setState({id: item_iid, name: item_name})
+      return item;
+    });
   }
 
   restoreItems(item) {
